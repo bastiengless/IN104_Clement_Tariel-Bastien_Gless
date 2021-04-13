d2d7c096d6e5ae568db1816ad4550d9e8a473675
Gless Bastien <bastien.gless@ensta-paristech.fr>
2021-04-13 15:59:50 +0200

    Implemented my classes (Bastien)

---
+++ OOP/classesTD2_bastien.py
@@ -0,0 +1,35 @@
+class Insect:
+	def __init__(self,size,is_social):
+		self.number_of_legs = 6
+		self.size = size
+		self.is_social = is_social # boolean
+
+class Hymenoptera(Insect):
+	# Bees and wasps
+	def __init__(self,size,is_social,colony_size,is_deadly):
+		self.colony_size = colony_size 
+		self.is_deadly = is_deadly
+		self.is_social = True
+		super().__init__(size, is_social)
+
+	def feedTheQueen(self):
+		print("Take my food, queen")
+
+	def sting(self):
+		if is_deadly:
+			print("One sting and you're dead")
+		else:
+			print("Just don't mess with me")
+
+class Coleoptera(Insect):
+	# All types of beetles
+	def __init__(self,size,is_social,horn_size,weight):
+		self.horn_size = horn_size
+		self.weight = weight
+		super().__init__(size, is_social)
+
+	def fightForFemale(self):
+		print("With my"+str(self.horn_size)"mm horn, I will defeat all my opponents to get to the female")
+
+	def flyClumsily(self):
+		print("I'm too heavy to fly correctly")
